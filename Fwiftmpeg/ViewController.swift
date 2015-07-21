4a4627eab3160297af5b46f29492eccc33615d5c
Son.Nguyen <Son@blanket.local>
2015-07-21 20:57:45 +1000

    Initial commit

---
+++ Fwiftmpeg/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fwiftmpeg
+//
+//  Created by Son.Nguyen on 21/07/2015.
+//  Copyright © 2015 sonng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
